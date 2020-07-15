fc4b4b03f5cd3a5c62445f5ee48acf37c6a2dad3
kihaller <67028582+kihaller@users.noreply.github.com>
2020-07-15 12:56:54 -0700

    Replaced searchbox plugin by original javascript code. Used mobile-first approach for lightbox plugin

--- js/app.js
+++ js/app.js
@@ -1,9 +1,47 @@
+/* jshint esversion: 6 */
+
 // Change fade duration
 lightbox.option({
   fadeDuration: 150,
   imageFadeDuration: 150,
+  disableScrolling: false,
 });
 
-$("input").search(function () {
-  //execute after done typing.
-}, 500);
+const largeScreen = window.matchMedia("(min-width: $breakpoint-tablet)");
+
+if (largeScreen) {
+  lightbox.option({
+    disableScrolling: true,
+  });
+}
+
+/* ============================================= */
+/* Search Bar functionality */
+/* ============================================= */
+
+//Retrieve all images at the beginning
+const allImages = document.getElementsByClassName("images");
+console.log(allImages);
+
+// Called on every keystroke
+function checkInput(event) {
+  console.log("checkInput function called.");
+
+  const searchTerm = document.getElementById("input").value.toLowerCase();
+  console.log("Currently in search bar: " + searchTerm);
+
+  for (let image of allImages) {
+    const dataSearch = image.getAttribute("data-search").toLowerCase();
+    const searchTermInImage = dataSearch.includes(searchTerm);
+    console.log(searchTermInImage);
+
+    if (searchTermInImage) {
+      image.style.display = "block";
+    } else {
+      image.style.display = "none";
+    }
+  }
+}
+
+// Register event listener that calls checkInput function every time user types sth. in input field
+document.getElementById("input").addEventListener("keyup", checkInput);
